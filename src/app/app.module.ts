28502cadf97d222ae7eb3d72d59de062587c4cf9
Sumit Bhoyar <sumitbhoyar@gmail.com>
2019-08-05 23:04:49 +0530

    Add modal for chat window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,6 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './fragments/header/header.component';
 import { LoginComponent } from './login/login.component';
 import { UserCardComponent } from './user-card/user-card.component';
+import { NavComponent } from './chat/nav/nav.component';
+import { MessageComponent } from './chat/message/message.component';
+import { DashboardComponent } from './chat/dashboard/dashboard.component';
 
 
 @NgModule({
@@ -13,11 +17,15 @@ import { UserCardComponent } from './user-card/user-card.component';
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    UserCardComponent
+    UserCardComponent,
+    NavComponent,
+    MessageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
